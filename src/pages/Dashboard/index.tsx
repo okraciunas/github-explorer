775bf4077470223aee4103c205c00e2c950b48b4
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-05-25 22:44:55 -0300

    Adiciona estilização para página Dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,57 @@
 import React, { FunctionComponent } from 'react'
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import logo from './../../assets/svg/logo.svg'
+import { Title, Form, Repositories } from './styles'
 
-const Dashboard: FunctionComponent = () => <Title>Dashboard</Title>
+const Dashboard: FunctionComponent = () => (
+  <>
+    <img src={logo} alt="Github Exloprer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/1759484?s=460&u=88225ada1b7c7214270a12002e8e25ae43213e9c&v=4"
+          alt="Leonardo Oliva Kraciunas"
+        />
+        <div>
+          <strong>okraciunas/github-explorer</strong>
+          <p>Aplicação React</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/1759484?s=460&u=88225ada1b7c7214270a12002e8e25ae43213e9c&v=4"
+          alt="Leonardo Oliva Kraciunas"
+        />
+        <div>
+          <strong>okraciunas/github-explorer</strong>
+          <p>Aplicação React</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/1759484?s=460&u=88225ada1b7c7214270a12002e8e25ae43213e9c&v=4"
+          alt="Leonardo Oliva Kraciunas"
+        />
+        <div>
+          <strong>okraciunas/github-explorer</strong>
+          <p>Aplicação React</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+)
 
 export default Dashboard
