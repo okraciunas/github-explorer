e05cfd3b8555d824ba9ed1d0141e90d441453b50
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-05-29 22:55:30 -0300

    Estilo da página Dashboard com requisição para a API do GitHub.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,57 +1,79 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, FormEvent, useState } from 'react'
 import { FiChevronRight } from 'react-icons/fi'
 
 import logo from './../../assets/svg/logo.svg'
-import { Title, Form, Repositories } from './styles'
+import { Title, Form, Repositories, Error } from './styles'
 
-const Dashboard: FunctionComponent = () => (
-  <>
-    <img src={logo} alt="Github Exloprer" />
-    <Title>Explore repositórios no Github</Title>
+import api from './../../services/api'
 
-    <Form>
-      <input placeholder="Digite o nome do repositório" />
-      <button type="submit">Pesquisar</button>
-    </Form>
+interface Repository {
+  id: number
+  full_name: string
+  description: string
+  owner: {
+    login: string
+    avatar_url: string
+  }
+}
 
-    <Repositories>
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/1759484?s=460&u=88225ada1b7c7214270a12002e8e25ae43213e9c&v=4"
-          alt="Leonardo Oliva Kraciunas"
+const Dashboard: FunctionComponent = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([])
+  const [searchRepository, setSearchRepository] = useState('')
+  const [inputError, setInputError] = useState('')
+
+  async function handleSearchRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault()
+
+    if (!searchRepository) {
+      setInputError('Digite o autor/nome do repositório.')
+      return
+    }
+
+    try {
+      const { data } = await api.get<Repository>(`repos/${searchRepository}`)
+      setRepositories([...repositories, data])
+      setSearchRepository('')
+      setInputError('')
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório.')
+    }
+  }
+
+  return (
+    <>
+      <img src={logo} alt="Github Exloprer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form onSubmit={handleSearchRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={searchRepository}
+          onChange={({ target }) => setSearchRepository(target.value)}
         />
-        <div>
-          <strong>okraciunas/github-explorer</strong>
-          <p>Aplicação React</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/1759484?s=460&u=88225ada1b7c7214270a12002e8e25ae43213e9c&v=4"
-          alt="Leonardo Oliva Kraciunas"
-        />
-        <div>
-          <strong>okraciunas/github-explorer</strong>
-          <p>Aplicação React</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
+      {inputError && <Error>{inputError}</Error>}
 
-      <a href="teste">
-        <img
-          src="https://avatars1.githubusercontent.com/u/1759484?s=460&u=88225ada1b7c7214270a12002e8e25ae43213e9c&v=4"
-          alt="Leonardo Oliva Kraciunas"
-        />
-        <div>
-          <strong>okraciunas/github-explorer</strong>
-          <p>Aplicação React</p>
-        </div>
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-)
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.id} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  )
+}
 
 export default Dashboard
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -43,6 +43,12 @@ export const Form = styled.form`
   }
 `
 
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
+`
+
 export const Repositories = styled.div`
   margin-top: 80px;
   max-width: 700px;
@@ -73,6 +79,7 @@ export const Repositories = styled.div`
 
     div {
       margin-left: 16px;
+      flex: 1;
 
       strong {
         font-size: 20px;
