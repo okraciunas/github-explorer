dd883f0f9ad3bdd54b2ad6c6487e1340d786d41c
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-05-31 19:20:04 -0300

    Estilização da página de informações do repositório.

--- src/pages/Repositories/index.tsx
+++ src/pages/Repositories/index.tsx
@@ -1,5 +1,10 @@
 import React, { FunctionComponent } from 'react'
-import { useRouteMatch } from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
+
+import logo from './../../assets/svg/logo.svg'
+
+import { Header, RepositoryInfo, Issues } from './styles'
 
 interface RepositoriesParams {
   repository: string
@@ -8,7 +13,54 @@ interface RepositoriesParams {
 const Repositories: FunctionComponent = () => {
   const { params } = useRouteMatch<RepositoriesParams>()
 
-  return <h1>Repositório: {params.repository}</h1>
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/1759484?v=4"
+            alt="Github Avatar"
+          />
+          <div>
+            <strong>okraciunas/github-explorer</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to={`dfgdfgd`}>
+          <div>
+            <strong>Titulo</strong>
+            <p>Descição</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  )
 }
 
 export default Repositories
